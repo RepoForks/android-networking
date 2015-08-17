fe49c96d708f14d49fa9ce78f81d1faca453e4c9
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-08-17 11:35:55 -0400

    added: moshi java object serialization

--- moshi/src/main/java/com/nullcognition/moshi/ActivityMain.java
+++ moshi/src/main/java/com/nullcognition/moshi/ActivityMain.java
@@ -6,19 +6,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.nullcognition.moshi.models.BlackjackHand;
+import com.nullcognition.moshi.models.Card;
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+import static com.nullcognition.moshi.models.Suit.*;
+
 public class ActivityMain extends Activity{
 
-	@Bind(R.id.txtV_parsedJson)
-	TextView parsedJson;
+	@Bind(R.id.txtV_result)
+	TextView txtV_result;
 
 	@OnClick(R.id.btn_parse)
 	public void onParseClick(){
@@ -45,7 +49,7 @@ public class ActivityMain extends Activity{
 		BlackjackHand              blackjackHand;
 		try{
 			blackjackHand = jsonAdapter.fromJson(json);
-			parsedJson.setText(blackjackHand.toString());
+			txtV_result.setText(blackjackHand.toString());
 		}
 		catch(IOException e){
 			e.printStackTrace();
@@ -71,11 +75,30 @@ public class ActivityMain extends Activity{
 		BlackjackHand              blackjackHand;
 		try{
 			blackjackHand = jsonAdapter.fromJson(json);
-			parsedJson.setText(blackjackHand.toString());
+			txtV_result.setText(blackjackHand.toString());
 		}
 		catch(IOException e){
 			e.printStackTrace();
 			Toast.makeText(ActivityMain.this, "conversion error", Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	@OnClick(R.id.btn_serialize)
+	public void onSerializeClick(){
+		BlackjackHand blackjackHand = new BlackjackHand(
+				new Card('1', CLUBS),
+				Arrays.asList(new Card('2', DIAMONDS), new Card('8', SPADES)));
+
+		Moshi moshi = new Moshi.Builder().build();
+		JsonAdapter<BlackjackHand> jsonAdapter = moshi.adapter(BlackjackHand.class);
+
+		String json;
+		try{
+			json = jsonAdapter.toJson(blackjackHand);
+			txtV_result.setText(json);
+		}
+		catch(IOException e){
+			e.printStackTrace();
 		}
 	}
 
