7ac364cae94b98e45fbc99d670cfb0f75506ab41
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-09-22 15:45:57 -0400

    added: retrofit example working, with rx integration which is not working

--- retrofit/src/main/java/com/nullcognition/retrofit/GitHub.java
+++ retrofit/src/main/java/com/nullcognition/retrofit/GitHub.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import retrofit.Call;
 import retrofit.http.GET;
+import retrofit.http.POST;
 import retrofit.http.Path;
+import rx.Observable;
 
 
 // both synchronous/async request use the same interface method call, use execute or enqueue for respective sync and async requests from the client
@@ -14,5 +16,37 @@ import retrofit.http.Path;
 public interface GitHub{
 
 	@GET("/users/{user}/repos") Call<List<Repo>> listRepos(@Path("user") String user);
+
+	// custom call adapters are allowed, the return type need not only be a Call
+	// with rx java integration you can return an observable with the call adapter
+
+	// to compare call with rx
+	@POST("/users/{user}/repos") Call<List<Repo>> loadRepoList(@Path("user") String user);
+
+	@POST("/users/{user}/repos") Observable<List<Repo>> loadRepoListRx(@Path("user") String user);
+
 }
 
+
+// baseUrl = "http://www.google.com/thisIsReplaced"
+// if the annotation parameter is without a leading '/' and the baseUrl ends with a /withoutTrailingBackSlash
+// then 'withoutTrailingBackSlash' the last file name will be replaced with the parameter
+
+// baseUrl = "http://www.google.com/annotationParameterAppended"
+// if the baseUrl ends with a folder, via the trailing '/' then the annotation paramemter will be appended to
+// the baseUrl
+
+// baseUrl = "http://www.google.com/thisIsDeleted/thisIsDeleted2
+// if the baseUrl has arguments, and the annotation parameter leads with '/' then the arguments are stripped
+// out and the url will be http://www.google.com/annotationParameter
+
+// recommended practice is to end the baseUrl with a '/'
+// and any @Url annotation parameters will start with a file or folder without a leading '/'
+
+// baseUrl = "http://www.google.com/
+// @GET("search/socks")
+
+// or use the the fully qualified path explicitly
+// @GET(http://www.google.com/search/socks")
+
+// OkHttp is auto depended on
--- retrofit/src/main/java/com/nullcognition/retrofit/MainActivity.java
+++ retrofit/src/main/java/com/nullcognition/retrofit/MainActivity.java
@@ -1,20 +1,29 @@
 package com.nullcognition.retrofit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.squareup.okhttp.HttpUrl;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit.BaseUrl;
 import retrofit.Call;
 import retrofit.Callback;
+import retrofit.MoshiConverterFactory;
 import retrofit.Response;
 import retrofit.Retrofit;
+import retrofit.RxJavaCallAdapterFactory;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
 // retrofit 2 - notes from:
 // http://inthecheesefactory.com/blog/retrofit-2.0/en
 // http://qiita.com/yongjhih/items/e0a4e3db04efcfa0ca2e
@@ -30,47 +39,106 @@ public class MainActivity extends AppCompatActivity{
 	HttpUrl  httpUrl;
 
 	GitHub github;
+	List<Repo> repos = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+//		Moshi moshi = new Moshi.Builder().add(SOME_OBJECT).build();
+//		then pass moshi into the create method for the converter factory
+
+		// if needing interceptors, create it in the okhttp level stack and pass
+		// the ok http client to the retrofit builder
+
 		retrofit = new Retrofit.Builder()
 				.baseUrl(API_URL)
-//				.addConverterFactory(MoshiConverterFactory.create()) // not working yet
-						// see https://github.com/square/retrofit/tree/397c10e4fc211c5fbbb474c5065a80daa05c4922/retrofit-converters
-// for a list of well known converters: json, xml, protobuf
+				.addConverterFactory(MoshiConverterFactory.create())
+				.addCallAdapterFactory(RxJavaCallAdapterFactory.create())
 				.build();
 
 		github = retrofit.create(GitHub.class);
 
-		synchronousCall();
+		Call<List<Repo>> call = github.listRepos("ersin-ertan");
 
+		Observable<List<Repo>> observable = github.loadRepoListRx("octocat");
+
+		// Call instances can be executed either synchronously or asynchronously. Each instance can only be used once,
+		// but calling clone() will create a new instance that can be used.
+
+		synchronousCall(call);
+		asynchronousCall(call.clone());
+		rxObservable(observable);
+	}
+	private void rxObservable(final Observable<List<Repo>> observable){
+
+		observable.observeOn(AndroidSchedulers.mainThread())
+		          .subscribe(new Subscriber<List<Repo>>(){
+			          @Override
+			          public void onCompleted(){
+				          Toast.makeText(getApplicationContext(), "Completed", Toast.LENGTH_SHORT).show();
+			          }
+
+			          @Override
+			          public void onError(Throwable e){
+				          Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+			          }
+
+			          @Override
+			          public void onNext(List<Repo> repoObject){
+				          Toast.makeText(getApplicationContext(), repoObject.get(0).name, Toast.LENGTH_SHORT).show();
+			          }
+		          });
 	}
 
-	private void synchronousCall(){
-
-		Call<List<Repo>> call = github.listRepos("octocat"); // throwing an exception here
-		// Caused by: java.lang.IllegalArgumentException: Unable to create converter for java.util.List<com.nullcognition.retrofit.Repo>
-
-		List<Repo> repos = null;
-
-		// synchronous
-
-		try{ repos = call.execute().body();}
-		catch(IOException e){ e.printStackTrace();}
-
-		((ListView) findViewById(R.id.listView)).setAdapter(new ArrayAdapter<Repo>(this, android.R.layout.simple_list_item_1, repos));
-
-		// asynchronous
+	private void asynchronousCall(final Call<List<Repo>> call){
 
 		call.enqueue(new Callback<List<Repo>>(){
 			@Override public void onResponse(final Response<List<Repo>> response){
-			//	response.body() // body() returns a List<Contributors>
+
+				// will be called even if there is a parse error, thus check for null
+				if(response.body() == null){
+					Log.d("MainActivity", response.errorBody().toString());
+					return;
+				}
+
+				repos = response.body();
+				List<String> strings = new ArrayList<String>(repos.size());
+				for(Repo r : repos){ strings.add(r.name);}
+
+				((ListView) findViewById(R.id.listView)).setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, strings));
+
+				Toast.makeText(MainActivity.this, "Async done", Toast.LENGTH_SHORT).show();
 			}
-			@Override public void onFailure(final Throwable t){ }
+			@Override public void onFailure(final Throwable t){ Toast.makeText(MainActivity.this, "Async Fail", Toast.LENGTH_SHORT).show();}
 		});
+
+//		call.cancel(); // to stop
+	}
+
+	private void synchronousCall(final Call<List<Repo>> call){ // network on main thread exception
+
+		new AsyncTask<Void, Void, Void>(){
+			@Override protected Void doInBackground(final Void... params){
+				try{ repos = call.execute().body();}
+				catch(IOException e){ e.printStackTrace();}
+
+				return null;
+			}
+			@Override protected void onPostExecute(final Void aVoid){
+				super.onPostExecute(aVoid);
+
+				List<String> strings = new ArrayList<String>(repos.size());
+				for(Repo r : repos){ strings.add(r.name);}
+
+				((ListView) findViewById(R.id.listView)).setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, strings));
+				Toast.makeText(MainActivity.this, "Sync done", Toast.LENGTH_SHORT).show();
+
+			}
+		}.execute();
+
+
 	}
 
 
